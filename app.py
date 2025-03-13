e286b2e9765d09c55fa7fe8991bddd7d14b3f798
codekookiz <codekookiz@gmail.com>
2025-03-13 16:06:26 +0900

    app.py 생성

---
+++ app.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sb
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
