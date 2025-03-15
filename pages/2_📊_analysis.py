faa93655d1784be20448e3ca41b8148b008b7882
nhchoi2 <choi1278@gmail.com>
2025-03-16 03:35:17 +0900

    Update 2_📊_analysis.py

--- pages/2_📊_analysis.py
+++ pages/2_📊_analysis.py
@@ -5,9 +5,10 @@ import seaborn as sns
 import matplotlib.font_manager as fm
 import os
 
-# 한글 폰트 설정 (Mac 기준)
-plt.rcParams['font.family'] = 'NanumGothic'
-plt.rcParams['axes.unicode_minus'] = False
+# 한글 폰트 설정 (배포 환경에서도 사용되는 한글 폰트로 설정)
+plt.rcParams['font.family'] = 'Malgun Gothic'  # Malgun Gothic 폰트 사용
+
+plt.rcParams['axes.unicode_minus'] = False  # 마이너스 기호 깨짐 방지
 
 # 데이터 로드
 df = pd.read_csv("data/고객db_전처리.csv")
