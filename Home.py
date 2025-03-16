dbca08ce41998a98ed9b897b5b24acda94294015
codekookiz <codekookiz@gmail.com>
2025-03-16 23:12:03 +0900

    오타 수정

--- Home.py
+++ Home.py
@@ -9,7 +9,7 @@
 
 
 # 중요! 스트림릿 실행 코드 : 터미널에서 streamlit run Home.py
-# 서버에서 돌아가는 것 확인해보고 싶으시면 : https://mini-project-1-23bmqdpdnx9ctd2y38o9nx.streamlit.app/
+# 서버에서 돌아가는 것 확인해보고 싶다면 : https://mini-project-1-23bmqdpdnx9ctd2y38o9nx.streamlit.app/
 
 
 import streamlit as st
